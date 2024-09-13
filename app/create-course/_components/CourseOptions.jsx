89661544678110ffdff9ac409098a47a97704451
JC MISA <johncarlomisa399@gmail.com>
2024-09-13 15:42:24 +0800

    add gemini api library

--- app/create-course/_components/CourseOptions.jsx
+++ app/create-course/_components/CourseOptions.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { ArrowBigUpDash, Brain, Clock, Hash } from 'lucide-react'
+import { UserInputContext } from '@/app/_context/UserInputContext';
+import { Input } from '@/components/ui/input';
 
 const CourseOptions = () => {
+
+    const { userCourseInput, setUserCourseInput } = useContext(UserInputContext);
+
+    const handleInputChange = (name, value) => {
+        setUserCourseInput(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
     return (
-        <div>CourseOptions</div>
+        <div className='px-10 md:px-20 lg:px-44'>
+            <div className='flex flex-col gap-3'>
+                <div className='flex flex-col sm:flex-row gap-3'>
+                    <div className='w-full sm:w-[50%]'>
+                        <div className='flex flex-row items-center gap-1 my-2'>
+                            <ArrowBigUpDash className='w-5 h-5' />
+                            <label className='text-sm'>Course Difficulty</label>
+                        </div>
+                        <Select onValueChange={(value) => handleInputChange('level', value)} defaultValue={userCourseInput?.level}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="Beginner" className='cursor-pointer'>Beginner</SelectItem>
+                                <SelectItem value="Intermediate" className='cursor-pointer'>Intermediate</SelectItem>
+                                <SelectItem value="Advance" className='cursor-pointer'>Advance</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className='w-full sm:w-[50%]'>
+                        <div className='flex flex-row items-center gap-1 my-2'>
+                            <Clock className='w-5 h-5' />
+                            <label className='text-sm'>Course Duration</label>
+                        </div>
+                        <Select onValueChange={(value) => handleInputChange('duration', value)} defaultValue={userCourseInput?.duration}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="1 Hour" className='cursor-pointer'>1 Hour</SelectItem>
+                                <SelectItem value="2 Hours" className='cursor-pointer'>2 Hours</SelectItem>
+                                <SelectItem value="2+ Hours" className='cursor-pointer'>2+ Hours</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='flex flex-row items-center gap-1'>
+                        <Hash className='w-5 h-5' />
+                        <label className='text-sm my-2'>No. of Chapters</label>
+                    </div>
+                    <Input type={'number'} onChange={(e) => handleInputChange('chapters', e.target.value)} defaultValue={userCourseInput?.chapters} />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -6,15 +6,21 @@ import CourseList from './_components/CourseList'
 import { UserInputContext } from '../_context/UserInputContext'
 import CourseTopic from './_components/CourseTopic'
 import CourseOptions from './_components/CourseOptions'
+import { Button } from '@/components/ui/button'
+import { Brain, LoaderCircle } from 'lucide-react'
 
 const CreateCoursePage = () => {
 
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
     const { userCourseInput, setUserCourseInput } = useContext(UserInputContext);
 
     useEffect(() => {
         console.log('courseInput Context: ', userCourseInput)
     }, [userCourseInput])
+
+    const generateAiCourse = async () => {
+
+    }
 
     return (
         <div className='p-5 mb-20'>
@@ -36,6 +42,17 @@ const CreateCoursePage = () => {
                     <div id='course-level' className='mt-60'>
                         <h2 className='text-center text-3xl my-5'>Select Course Options</h2>
                         <CourseOptions />
+                    </div>
+                )
+            }
+            {
+                (userCourseInput?.level != null && userCourseInput?.duration != null && userCourseInput?.chapters != null) && (
+                    <div className='px-10 md:px-20 lg:px-44 mt-5 float-end'>
+                        <Button className='min-w-40 max-w-40 flex flex-row items-center gap-2' onClick={() => generateAiCourse()}>
+                            {
+                                loading ? <LoaderCircle className='animate-spin' /> : <>Generate Course <Brain /></>
+                            }
+                        </Button>
                     </div>
                 )
             }
