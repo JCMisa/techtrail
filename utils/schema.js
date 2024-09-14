8c63ad44574f24bf469491148cf6069f3df9a346
JC MISA <johncarlomisa399@gmail.com>
2024-09-14 12:50:41 +0800

    setup backend with drizzle orm and neon db

---
+++ utils/schema.js
@@ -0,0 +1,20 @@
+import {
+    boolean,
+    json,
+    pgTable,
+    serial,
+    varchar,
+} from "drizzle-orm/pg-core";
+
+export const Course = pgTable("course", {
+    id: serial("id").primaryKey(),
+    courseId: varchar("courseId").notNull(),
+    courseName: varchar("courseName"),
+    courseTitle: varchar("courseTitle"),
+    level: varchar("level"),
+    courseOutput: json("courseOutput"),
+    createdBy: varchar("createdBy"),
+    username: varchar("username"),
+    userProfileImage: varchar("userProfileImage"),
+    courseBanner: varchar('courseBanner')
+});
