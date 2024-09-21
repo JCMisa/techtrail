31d7652c7300f6f6f4c8a2c413022bc5438b295f
JC MISA <johncarlomisa399@gmail.com>
2024-09-21 12:20:12 +0800

    add player schema, and add the logged in user as a player in Player schema

--- app/page.js
+++ app/page.js
@@ -1,6 +1,54 @@
+'use client'
+
+import { useUser } from "@clerk/nextjs";
 import Header from "./_components/Header";
+import { useEffect } from "react";
+import { db } from "@/utils/db";
+import { Player } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import moment from "moment";
+import { toast } from "sonner";
 
 export default function Home() {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && addPlayer();
+  }, [user])
+
+  const addPlayer = async () => {
+    try {
+      const existingPlayer = await db.select()
+        .from(Player)
+        .where(eq(Player?.email, user?.primaryEmailAddress?.emailAddress));
+
+      if (existingPlayer.length > 0) {
+        toast(
+          <p className='text-yellow-500 text-sm font-bold'>Player already exists!</p>
+        )
+        return;
+      } else {
+        const result = await db.insert(Player).values({
+          email: user?.primaryEmailAddress?.emailAddress,
+          name: user?.fullName,
+          image: user?.imageUrl,
+          createdAt: moment().format('MM-DD-yyyy'),
+          points: 0,
+          username: user?.firstName
+        })
+        if (result) {
+          toast(
+            <p className='text-green-500 text-sm font-bold'>Player added successfully!</p>
+          )
+        }
+      }
+    } catch (error) {
+      toast(
+        <p className='text-red-500 text-sm font-bold'>Internal error occured while saving the player</p>
+      )
+    }
+  }
+
   return (
     <div>
       <Header />
--- utils/schema.js
+++ utils/schema.js
@@ -1,5 +1,5 @@
 import {
-    boolean,
+    integer,
     json,
     pgTable,
     serial,
@@ -18,4 +18,14 @@ export const Course = pgTable("course", {
     username: varchar("username"),
     userProfileImage: varchar("userProfileImage"),
     courseBanner: varchar('courseBanner')
+});
+
+export const Player = pgTable("player", {
+    id: serial("id").primaryKey(),
+    email: varchar("email"),
+    name: varchar("name"),
+    image: varchar("image"),
+    createdAt: varchar("createdAt"),
+    points: integer("points"),
+    username: varchar("username")
 });
